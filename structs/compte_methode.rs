13779dd4844358364c60855ad46a6d4cb89a29e9
Yves <sevyns.pro@gmail.com>
2026-07-08 23:15:06 +0200

    Des exercices sur les structs

---
+++ structs/compte_methode.rs
@@ -0,0 +1,8 @@
+/*
+
+Consigne : reprends un struct Compte (titulaire: String, solde: u32). Ajoute deux méthodes : deposer qui augmente le solde d'un montant, et afficher qui affiche le solde. Dans main, dépose deux fois puis affiche.
+Résultat attendu :
+
+Solde de Bob : 250
+
+*/
