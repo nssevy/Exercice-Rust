cfc9f4bbcf9d1e87f9dd1b0a001c024d8702b10e
Yves <sevyns.pro@gmail.com>
2026-07-09 14:57:52 +0200

    La révision de la fonction pour additionner un tableau

---
+++ revisions/additionnerTableau.rs
@@ -0,0 +1,20 @@
+fn addition_tab(a: &[i32]) -> i32 {
+    let mut i: usize = 0;
+    let mut stock: i32 = 0;
+
+    while i < a.len() {
+        let valeur = a[i];
+        stock += valeur;
+
+        i += 1;
+    }
+    return stock;
+}
+
+fn main() {
+    let v: Vec<i32> = vec![10, 20, 30];
+
+    let total = addition_tab(&v);
+
+    println!("{total}");
+}
