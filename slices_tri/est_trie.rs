8d9623446bda9702d555d3f1c3f8c2208c69e66f
Yves <sevyns.pro@gmail.com>
2026-07-14 07:26:45 +0200

    Refactor du code où on a fait une fonction croissant et une fonction décroissant qui permet d'appeler directement ces deux fonctions-là dans la fonction Vérificateur de tableau dont le seul rôle est d'appeler ses fonctions en question ou de dire si le tableau est trié ou pas trié ou si il est vide. Mais le code n'est pas encore fonctionnel Ne fonctionne pas encore pour les tableaux patrillés et les tableaux à doublons.

--- slices_tri/est_trie.rs
+++ slices_tri/est_trie.rs
@@ -8,22 +8,66 @@
 
 */
 
-fn tab_croissant(a: &[i32]) -> u32 {
-	
+fn croissant (c: &[i32]) -> i32 {
+
 	let mut i: usize = 0;
-	let mut croissant: u32 = 0;
-	
-	while i < a.len()-1 {
-	
-		if a[i] < a[i+1] {
-			croissant = 1;
-			i+=1
-		} else {
-			croissant = 0
+	let mut valeur: i32 = 0;
+
+	while i < c.len()-1 {
+
+		if c[i] <= c[i+1]{
+			valeur = 1;
+			i+=1;
 		}
+
+        }
+
+	valeur
+}
+
+fn decroissant (d: &[i32]) -> i32 {
+
+        let mut i: usize = 0;
+        let mut valeur: i32 = 0;
+
+        while i < d.len()-1 {
+
+                if d[i] >= d[i+1]{
+                        valeur = 2;
+                        i+=1;
+                }
+
+        }
+
+        valeur
+}
+
+fn verificateur_de_tableau(a: &[i32]) -> i32 {
+	/*
+	Dans la fonction les valeurs de croissant sont défini par : 
 	
+	0 = tableau pas trier
+	1 = Tableau dans l'ordre croissant
+	2 = Tableau décroissant
+	3 = Tableau vide ou pas assez d'éléments pour comparer (au mininmum 2)
+	*/
+	
+	let i: usize = 0;
+	let mut valeur_retour: i32 = 0;
+
+	if a.len() < 2 {
+		valeur_retour  = 3;
+		return valeur_retour;
 	}
-	croissant
+	
+	if a[i] < a[i+1] {
+		croissant(&a)
+	} else if a[i] > a [i+1]{
+		decroissant(&a)
+	} else {
+		valeur_retour = 0;
+		return valeur_retour;
+	}
 	
 }
 
@@ -32,8 +76,19 @@ fn main () {
 	let tab_trier: Vec<i32> = vec![23, 56, 65, 91];
 	
 	let tab_pas_trier: Vec<i32> = vec![2, 23, 2, 34, 56];
-	
-	println!("{:?}", tab_croissant(&tab_trier));	
 
+	let _tab_doublon: Vec<i32> = vec![23, 23, 34, 56];
+
+	let aleatoire: Vec<i32> = vec![];
+
+	let decroissant: Vec<i32> = vec![100, 50, 25, 1];
+	
+	println!("{:?}", verificateur_de_tableau(&tab_trier));	
+ 
+	println!("{:?}", verificateur_de_tableau(&decroissant));
+	
+	println!("{:?}", verificateur_de_tableau(&aleatoire));
+
+	println!("{:?}", verificateur_de_tableau(&tab_pas_trier));
 }
 
