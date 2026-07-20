8b99a2537533fc438edb9c29769374e5d76b88c3
Yves <sevyns.pro@gmail.com>
2026-07-20 04:59:52 +0200

    Afficher tableau fonctionne

--- echecs/echecs.rs
+++ echecs/echecs.rs
@@ -1,5 +1,5 @@
 /*
-représente un échiquier de 8×8 cases, entièrement vide au départ (. partout), puis place quelques pièces 
+représente un échiquier de 8×8 cases, entièrement vide au départ (. partout), puis place quelques pièces
 la main (par exemple un R et un T). Enfin, affiche le plateau ligne par ligne dans le terminal.
 Résultat attendu à l'exécution (l'emplacement exact des pièces t'appartient, c'est juste un exemple) :
 . . . . . . . .
@@ -23,6 +23,7 @@ enum PieceType {
 }
 
 impl PieceType {
+    // Renvoie les lettres des pieces.
     fn piece_en_lettre(&self) -> char {
         match self {
             PieceType::Roi => 'R',
@@ -36,64 +37,50 @@ impl PieceType {
     }
 }
 
-fn echequier(){
+fn echequier() {
     // variable pour les boucles.
     let mut l: usize = 1;
     let mut c: usize = 1;
     let mut i: usize = 0;
     let mut j: usize = 0;
 
-    // Variables des pices.
+    // Pieces.
     let vide = PieceType::Vide.piece_en_lettre();
 
-    // Variables des tableaux pour les lignes et colonne.
+    // Initialisation des deux tableaux vides.
     let mut ligne = vec![];
-    let mut colonne = vec![]; 
+    let mut colonne = vec![];
 
-    while l <= 8{
+    // rempli la premiere ligne du tableau
+    while l <= 8 {
         ligne.push(vide);
-        l+= 1;
+        l += 1;
     } 
-
-    // println!("{:?}", ligne); 
-
+    
+    // Tableau tridimentionnel, rempli le tebleau à chaque index du tableau ligne.
     while c <= 8 {
         colonne.push(ligne.clone());
         c += 1;
     }
 
-    // println!("{:?}", colonne);
-     
-    /* while i <= 8 {
-        println!("{:?}", colonne[i].iter());
-        i += 1;
-    } */
-
-   
-
-        /* while i < 8 {
-            print!("{}", colonne[i][j]);  
-                   
-            i+= 1; 
-        } */
-
-        while i < 8 {
-            while j < 8 {
-                print!("{}", colonne[i][j]);
-                j += 1;
-            }
-            println!();
-            i+= 1;
+    /* Affiche le plateau (tableau)
+    * i = index 0
+    * j = les éléments à l'interrieur de i.
+    */
+    while i < 8 {
+        while j < 8 {
+            print!("{} ", colonne[i][j]);
+            j += 1;
         }
-     
-   
+        // Retour à la ligne
+        println! {};
+        // Reset j pour re-parcourir de l'index 0
+        j = 0;
+        i += 1;
+    }
 }
 
 fn main() {
     let _roi = PieceType::Roi.piece_en_lettre();
-    // let vide = PieceType::Vide.piece_en_lettre();
-
-    // let _echequier = vec![vide];
-
     echequier();
 }
