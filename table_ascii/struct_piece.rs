a4bca7289657bf442caeb8a84116f104e204cde9
Yves <sevyns.pro@gmail.com>
2026-07-21 11:12:04 +0200

    ajout d'un impl new

--- table_ascii/struct_piece.rs
+++ table_ascii/struct_piece.rs
@@ -2,23 +2,25 @@
 reprends tes enums Couleur (Blanc, Noir) et TypePiece (Roi, Dame, Tour, Fou, Cavalier, Pion). Définis une struct Piece avec deux champs : couleur de type Couleur, et type_piece de type TypePiece. Dans le main, crée à la main (instancie) une pièce représentant la dame blanche, puis affiche-la entièrement avec {:?}.
 Résultat attendu :
 Piece { couleur: Blanc, type_piece: Dame }
+
+ajoute un bloc impl Piece contenant une méthode statique new qui prend deux paramètres, couleur: Couleur et type_piece: TypePiece, et renvoie la Piece correspondante (-> Piece). Dans le main, remplace ta création manuelle par un appel à Piece::new(...) pour fabriquer la dame blanche, puis affiche-la avec {:?}.
 */
 #![allow(dead_code)]
 
 #[derive(Debug)]
 enum Couleur {
-    Blanc,
-    Noir,
+    Blanc, //majuscule
+    Noir, //minuscule
 }
 
 #[derive(Debug)]
 enum TypePiece {
-    Roi,
-    Dame,
-    Tour,
-    Fou,
-    Cavalier,
-    Pion,
+    Roi, //k
+    Dame, //q
+    Tour, //r
+    Fou,//b
+    Cavalier,//n
+    Pion,//p
 }
 
 #[derive(Debug)]
@@ -27,7 +29,17 @@ struct Piece {
     type_piece: TypePiece,
 }
 
+impl Piece{
+    fn new(couleur: Couleur,type_piece: TypePiece) -> Piece{
+       Piece {couleur, type_piece} 
+    }
+}
+
 fn main() {
-    let dame_blanche = Piece { couleur: Couleur::Blanc, type_piece: TypePiece::Dame };
-    println!("{:?}", dame_blanche);
+    let dame = Piece::new(Couleur::Blanc, TypePiece::Dame );
+
+    let roi = Piece::new(Couleur::Noir, TypePiece::Roi);
+
+    println!("{:?}", dame);
+    println!("{:?}", roi);
 }
