5cb919ba3325937f4bd70178b773403e144e50ea
Yves <sevyns.pro@gmail.com>
2026-07-23 23:25:21 +0200

    Fonction qui renvoie la couleur de la pièce fonctionnelle.

--- echecs/creation_piece.rs
+++ echecs/creation_piece.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 #[derive(Debug)]
 enum TypePiece {
     Roi, //K ing
@@ -20,37 +22,24 @@ struct Piece {
     type_piece : TypePiece,
 }
 
-// Le probleme ce que ca prends en entre des lettre majusucles, des lettres minuscule
-// et des chiffres
-// Les chiffres doivent etr ignorer et renvoyer null
-
-/* impl Piece {
-    fn creation_de_piece(a: char) -> Piece{
-        match_lettre(&a);
+fn couleur_piece(a: char) -> Option<Couleur> {
+    if a.is_ascii_uppercase() {
+        Some(Couleur::Blanc)
+    } else if a.is_ascii_lowercase() {
+        Some(Couleur::Noir)
+    } else {
+        None
     }
+}
 
-    fn match_lettre(a: char) -> Option<Piece::TypePiece> {
-        match a {
-            'K' | 'k' => TypePiece::Roi,
-            'Q' | 'q' => TypePiece::Dame,
-            'N' | 'n' => TypePiece::Cavalier,
-            'T' | 't' => TypePiece::Tour,
-            'B' | 'b' => TypePiece::Fou,
-            'P' | 'p' => TypePiece::Pion,
-            _ => Null,
-        }
-    }
-
-    fn blanc_ou_noir(a: char) -> Option<Piece::Couleur>{
-        if a.is_ascii_uppercase() == 
-    }
+fn type_piece(a: char) -> Option<TypePiece>{
 
 }
 
-*/
-
 fn main() {
-    let roi = Piece{couleur: Couleur::Blanc, type_piece:TypePiece::Roi};
+    let a = couleur_piece('7');
+    println!("{:?}", a);
+    let roi = Piece{couleur: couleur_piece('A').unwrap(), type_piece:TypePiece::Roi};
     println!("{:?}", roi);
    
 }
