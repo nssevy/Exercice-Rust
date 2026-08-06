67af029f71bac957bd1bdd757ad0ca90e5c1ffbc
Yves <sevyns.pro@gmail.com>
2026-08-06 10:07:32 +0700

    exercice compte en banque

--- examples/quand_utiliser_compte_banquaire.rs
+++ examples/quand_utiliser_compte_banquaire.rs
@@ -1,5 +1,34 @@
 /*
-Un compte bancaire caractérisé par un numéro, un solde et le nom du titulaire — et on veut pouvoir
+Un compte bancaire caractérisé par un numéro, un solde et le nom du titulaire et on veut pouvoir
 y déposer et retirer de l'argent.
  */
-fn main() {}
+#[derive(Debug)]
+#[allow(unused)]
+struct Compte {
+    numero: u32,
+    solde: i32,
+    nom_titulaire: String
+}
+
+impl Compte{
+    fn deposer(&mut self, a: &i32) {
+        self.solde += a;
+    }
+
+    fn retirer(&mut self, a: &i32) {
+        self.solde -= a;
+    }
+}
+
+fn main() {
+    let mut sevy = Compte{
+        numero: 9234,
+        solde: 0,
+        nom_titulaire: String::from("sevy")
+    };
+
+    (&mut sevy).deposer(&10);
+    (&mut sevy).retirer(&5);
+
+    println!("{:?}", sevy);
+}
